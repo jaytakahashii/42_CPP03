85828246da735434e9d40634605342088e74e114
jtakahas <jtakahas@student.42tokyo.jp>
2025-04-11 14:41:04 +0900

    refactor(SRC): ♻️  edit message

--- ex01/src/ScavTrap.cpp
+++ ex01/src/ScavTrap.cpp
@@ -14,14 +14,20 @@ ScavTrap::~ScavTrap() {
 }
 
 void ScavTrap::attack(const std::string& target) {
-  if (_energyPoints > 0 && _hitPoints > 0) {
-    std::cout << "ScavTrap " << _name << " attacks " << target << ", causing "
-              << _attackDamage << " points of damage!" << std::endl;
-    _energyPoints--;
-  } else {
-    std::cout << "ScavTrap " << _name << " has no energy or hit points left!"
-              << std::endl;
+  if (_hitPoints == 0) {
+    std::cout << RED "ScavTrap " << _name
+              << " is already down and cannot attack!" RESET << std::endl;
+    return;
   }
+  if (_energyPoints == 0) {
+    std::cout << RED "ScavTrap " << _name
+              << " has no energy left and cannot attack!" RESET << std::endl;
+    return;
+  }
+  _energyPoints--;
+  std::cout << YELLOW "ScavTrap " << _name << " attacks " << target
+            << ", causing " << _attackDamage << " points of damage!" RESET
+            << std::endl;
 }
 
 void ScavTrap::guardGate() {
